22dc25704f657d4ca587797cc220e67600cfc65d
saqlainish <saqlainsj1@gmail.com>
2023-07-07 23:54:08 +0500

    First commit social app UI react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
+import './styles.css'
+
+
+function App() {
+  return(
+    <div> 
+      <Register />
+      <Login />
+    </div>
+  )
+}
+
+export default App
