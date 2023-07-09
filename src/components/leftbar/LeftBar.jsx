cc56c423314879da9da518644027c314fea70ec0
saqlain-hub <saqlainsj12@gmail.com>
2023-07-09 13:25:49 +0500

    create pretty decent UI for social media app but work is remaining

--- src/components/leftbar/LeftBar.jsx
+++ src/components/leftbar/LeftBar.jsx
@@ -44,6 +44,59 @@ const LeftBar = () => {
             <img src={Memories} alt="" />
             <span>Memories</span>
           </div>
+        </div>
+        <hr/>
+        <div className="menu">
+          <span>Your shortcuts</span>
+          <div className="item">
+            <img src={Events} alt="" />
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <img src={Gaming} alt="" />
+            <span>Gaming</span>
+          </div>
+          <div className="item">
+            <img src={Gallery} alt="" />
+            <span>Gallery</span>
+          </div>
+          <div className="item">
+            <img src={Videos} alt="" />
+            <span>Videos</span>
+          </div>
+          <div className="item">
+            <img src={Messages} alt="" />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr/>
+        <div className="menu">
+          <span>Others</span>
+          <div className="item">
+            <img src={Fund} alt="" />
+            <span>Fundraiser</span>
+          </div>
+          <div className="item">
+            <img src={Tutorials} alt="" />
+            <span>Tutorials</span>
+          </div>
+          <div className="item">
+            <img src={Courses} alt="" />
+            <span>Courses</span>
+          </div>
+
+          <div className="item">
+            <img src={Courses} alt="" />
+            <span>Courses</span>
+          </div>
+          <div className="item">
+            <img src={Courses} alt="" />
+            <span>Courses</span>
+          </div>
+          <div className="item">
+            <img src={Courses} alt="" />
+            <span>Courses</span>
+          </div>
 
         </div>
       </div>
--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -1,7 +1,38 @@
 import './rightBar.scss'
+import profileImg from '../../assets/img-2.jpg'
 const RightBar = () => {
   return (
-    <div>RightBar</div>
+    <div className='rightBar'>
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={ profileImg } alt="" />
+              <span>John Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={ profileImg } alt="" />
+              <span>John Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 
