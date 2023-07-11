8b6d5bb8eda2112aa3286e1d5ed1548b3b3a8e34
saqlain-hub <saqlainsj12@gmail.com>
2023-07-12 02:12:44 +0500

    Comment UI almost done interrupted by electricity tripping in Pakistan

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import userPic from '../../assets/img-2.jpg';
+import femalePic from '../../assets/img-4.jpg';
+
+const Comments = () => {
+
+    const comments = [
+        {
+            id: 1,
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam itaque laboriosam atque sed! Id aspernatur reiciendis placeat, quo nam eum sint dolores enim praesentium reprehenderit.",
+            name: 'John Doe',
+            userId: 2,
+            profilePicture: userPic
+        },
+        {
+            id: 2,
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam itaque laboriosam atque sed! Id aspernatur reiciendis placeat, quo nam eum sint dolores enim praesentium reprehenderit.",
+            name: 'Jane Doe',
+            userId: 2,
+            profilePicture: femalePic
+        },
+    ];
+
+  return (
+    <div className='comments'>
+        {
+            comments.map(comment => (
+                <div className="comment">
+                    <img src={comment.profilePicture} alt="user profile pic" />
+                    <div className="info">
+                        <span>{comment.name}</span>
+                        <p>{comment.desc}</p>
+                    </div>
+                    <span className="date">1 hour ago</span>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Comments
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,12 +1,15 @@
 import './post.scss' 
+import { useState } from 'react';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined'
 import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined'
 import ShareOutlinedIcon  from '@mui/icons-material/ShareOutlined';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 import { Link } from 'react-router-dom';
+import Comments from '../comments/Comments'
 
 const Post = ({ post }) => {
+    const [commentOpen, setCommentOpen] = useState(false)
 
     // Temporary
     const liked = false;
@@ -35,7 +38,7 @@ const Post = ({ post }) => {
                     {liked ? <FavoriteOutlinedIcon/>: <FavoriteBorderOutlinedIcon/>}
                     12 Likes
                 </div>
-                <div className="item">
+                <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
                     <TextsmsOutlinedIcon/>
                     5 Comments
                 </div>
@@ -44,6 +47,7 @@ const Post = ({ post }) => {
                     Share
                 </div>
             </div>
+            { commentOpen && <Comments /> }
         </div>
     </div>
   )
