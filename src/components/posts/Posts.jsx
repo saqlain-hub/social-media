6d4431ca817aadf04fd7b8332ab1df4a165d0a65
saqlain-hub <saqlainsj12@gmail.com>
2023-07-11 16:03:24 +0500

    Post UI almost completed

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,8 +1,35 @@
 import "./posts.scss"
+import postImg from '../../assets/image-2.jpg'
+import profileImg from '../../assets/img-3.jpg'
+import Post from '../../components/post/Post'
 
 const Posts = () => {
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic: profileImg,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: postImg
+    },
+
+    {
+      id: 2,
+      name: "John Doe",
+      userId: 2,
+      profilePic: profileImg,
+      desc: "amet consectetur adipisicing elit. Libero recusandae eius voluptatibus, maxime voluptates nesciunt",
+      img: postImg
+    }
+  ];
+
   return (
-    <div className='posts'>Posts</div>
+    <div className='posts'>
+      { posts.map(post => (
+        <Post post={post} key={post.id}/>
+      ))}
+    </div>
   )
 }
 
--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -36,9 +36,10 @@ const Stories = () => {
         <div className="story">
             <img src={currentUser.profilePic} alt="" />
             <span>{currentUser.name}</span>
+            <button>+</button>
         </div>
         { stories.map(story => (
-            <div className="story">
+            <div className="story" key={story.id}>
                 <img src={story.img} alt="" />
                 <span>{story.name}</span>
             </div>
