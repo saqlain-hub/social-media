68c1d99865f67a64751d2a568f00b8c59686f15d
saqlain-hub <saqlainsj12@gmail.com>
2023-07-11 15:11:39 +0500

    Stories and posts section almost done

--- src/components/leftbar/LeftBar.jsx
+++ src/components/leftbar/LeftBar.jsx
@@ -13,16 +13,21 @@ import Messages from '../../assets/10.png'
 import Fund from '../../assets/11.png'
 import Tutorials from '../../assets/12.png'
 import Courses from '../../assets/13.png'
+import { AuthContext } from '../../context/authContext'
+import { useContext } from 'react';
 
 
 const LeftBar = () => {
+
+  const { currentUser } = useContext(AuthContext)
+
   return (
     <div className='leftBar'>
       <div className="container">
         <div className="menu">
           <div className="user">
-            <img src={profileImg} alt="" />
-            <span>John Doe</span>
+            <img src={currentUser.profilePic} alt="" />
+            <span>{currentUser.name}</span>
           </div>
           <div className="item">
             <img src={Friends} alt="" />
---
+++ src/components/stories/Stories.jsx
@@ -0,0 +1,51 @@
+import "./stories.scss"
+import profileImg from '../../assets/image-2.jpg'
+import { useContext } from "react"
+import {AuthContext } from '../../context/authContext'
+
+const Stories = () => {
+
+    const { currentUser } = useContext(AuthContext);
+
+    /// TEMPORARY
+    const stories = [
+        {
+            id: 1,
+            name: "Ava Angelina",
+            img: profileImg
+        },
+        {
+            id: 2,
+            name: "Dani Adams",
+            img: profileImg
+        },
+        {
+            id: 3,
+            name: "Kimmy Samuel",
+            img: profileImg
+        },
+        {
+            id: 4,
+            name: "Mia Handerix",
+            img: profileImg
+        }
+    ]
+
+  return (
+    <div className='stories'>
+        <div className="story">
+            <img src={currentUser.profilePic} alt="" />
+            <span>{currentUser.name}</span>
+        </div>
+        { stories.map(story => (
+            <div className="story">
+                <img src={story.img} alt="" />
+                <span>{story.name}</span>
+            </div>
+        ))}
+
+    </div>
+  )
+}
+
+export default Stories
